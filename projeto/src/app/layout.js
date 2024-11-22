ee51c8b41b259eece7849b07aa88833d6c4e4b57
leticiaandradef <lelectpm2021@gmail.com>
2024-11-22 19:25:36 -0400

    projeto

---
+++ projeto/src/app/layout.js
@@ -0,0 +1,21 @@
+import "./globals.css";
+import Header from "@/components/Header";
+
+export const metadata = {
+  title: "Sanrio",
+  description: "Marca",
+  charset: 'UTF-8',
+  author: 'Leticia Andrade',
+  keywords: 'Marca, Life style',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt">
+      <body>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ projeto/src/app/page.js
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Image className={styles.img} src='/image/sanrio.png' alt="Imagem Logo sanrio" width={300} height={200}/>
+    </div>
+);
+}
